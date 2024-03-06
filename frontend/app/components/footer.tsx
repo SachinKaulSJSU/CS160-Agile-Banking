d74bfddcdb59fd0307583ff30acd8bab5294f31f
BrianTDo <brian18do@gmail.com>
2024-03-06 05:19:01 -0800

    basic home page

---
+++ frontend/app/components/footer.tsx
@@ -0,0 +1,13 @@
+export default function () {
+  return (
+    <footer>
+      <div className="bg-gray-50">
+        <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+          <div className="mr-auto place-self-center lg:col-span-7">
+            
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,14 @@
-import Image from "next/image";
+import Header from './components/header'
+import OfferSection from './components/offer'
+import HeroSection from './components/hero'
+import Footer from './components/footer'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
+        <Header/>
+        <HeroSection/>
+        <Footer/>
     </main>
   );
 }
